8dfb58df0a6fdaa359b1cbeb1dc71f3aa3ce8f6e
최현우 <danielch0401@naver.com>
2020-07-17 21:33:53 +0900

    Add Comment
    Add app.js comment

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ import next from 'next'
 import cors from 'cors'
 import morgan from 'morgan'
 import expressSession from 'express-session'
-import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 //import redisStore from 'connect-redis' Redis session 추후 추가 예정
 
 import v1 from './routes/v1/v1'
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production'; // production setting
 const prod = process.env.NODE_ENV === 'production';
 
 const app = next({dev})
 const handle = app.getRequestHandler()
 
-const swaggerDefinition = {
+const swaggerDefinition = { // swagger main definition
     info: {
         title: 'Auth Service',
         version: '0.1',
@@ -25,7 +25,7 @@ const swaggerDefinition = {
     basePath: '/'
 };
 
-const options = {
+const options = { // swagger option
     swaggerDefinition,
     apis: ['./routes/v1/api.js']
 };
@@ -37,12 +37,12 @@ app.prepare().then(() => {
     
     server.set('port', process.env.PORT || 5000)
 
-    server.use(morgan('dev'));
-    server.use(express.json())
+    server.use(morgan('dev')); // 로그 찍어주는 모듈 (dev) 사용
+    server.use(express.json()) // json 사용
     server.use(express.urlencoded({ extended: true }));
-    server.use(cors())
+    server.use(cors()) // cors 세팅
     server.use(
-        expressSession({
+        expressSession({ // 세션 세팅
             resave: false,
             saveUninitialized: false,
             secret: process.env.COOKIE_SECRET,
@@ -54,9 +54,9 @@ app.prepare().then(() => {
         })
     )
 
-    server.use('/v1', v1)
+    server.use('/v1', v1) // /v1으로 라우팅
 
-    server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // /api-docs로 들어가면 swagger-api로 라우팅 
 
     server.get('*', (req, res) => {
         res.sendStatus(404)
